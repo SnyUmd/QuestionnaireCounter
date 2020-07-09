4a793a86592f1fb58071c3e0d7b61351e2f0e0ff
DESKTOP-3JEKU91\sny-acer <sny.umd@gmail.com>
2020-07-10 01:18:28 +0900

    no message

--- QuestionnaireCounter/QuestionnaireCounter/MainWindow.xaml.cs
+++ QuestionnaireCounter/QuestionnaireCounter/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+
 namespace QuestionnaireCounter
 {
     /// <summary>
@@ -22,6 +25,25 @@ namespace QuestionnaireCounter
     {
         common cls_comm = new common();
         bool blRunSts = false;
+
+
+        //*************************************************************************
+        //*************************************************************************
+        //デバッグ
+        //*************************************************************************
+        //*************************************************************************
+        private void Click_Debug(object sender, RoutedEventArgs e)
+        {
+            //SetList(ref cls_comm.LstAnswer, 10, 5);
+            CtrlEn(!GridAnswerBtn.IsEnabled);
+        }
+
+        //*************************************************************************
+        //*************************************************************************
+        //*************************************************************************
+        //*************************************************************************
+
+
 
         //*************************************************************************
         public MainWindow()
@@ -33,9 +55,48 @@ namespace QuestionnaireCounter
             SetList(ref cls_comm.LstAnswer, 10, 5);
         }
 
+        //***************************************************************
+        public void PerformClick2(Button self)
+        {
 
+            var peer = new ButtonAutomationPeer(self);
+            var provider = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
 
+            provider.Invoke();
+        }
 
+        //*************************************************************************
+        private void SetList(ref List<int[]> lst, int ix, int iy)
+        {
+            int[] ary_int_val;
+
+            ary_int_val = new int[iy];
+            for (int i = 0; i < iy; i++)
+                ary_int_val[i] = 0;
+
+            for (int i = 0; i < ix; i++)
+            {
+                lst.Add(ary_int_val);
+            }
+        }
+
+        //*************************************************************************
+        public void CtrlEn(bool bl_Enable)
+        {
+            GridAnswerBtn.IsEnabled = bl_Enable;
+            GridList.IsEnabled = bl_Enable;
+            GridUpDown.IsEnabled = bl_Enable;
+
+            blRunSts = bl_Enable;
+        }
+
+        //*************************************************************************
+        //*************************************************************************
+        //*************************************************************************
+        //コントローラのイベント
+        //*************************************************************************
+        //*************************************************************************
+        //*************************************************************************
         //*************************************************************************
         private void Click_BTN(object sender, RoutedEventArgs e)
         {
@@ -60,39 +121,41 @@ namespace QuestionnaireCounter
             }
         }
 
+
+
         //*************************************************************************
-        private void SetList(ref List<int[]> lst, int ix, int iy)
+        private void GetKeyDown(object sender, KeyEventArgs e)
         {
-            int[] ary_int_val;
+            if (!blRunSts)
+                return;
 
-            ary_int_val = new int[iy];
-            for (int i = 0; i < iy; i++)
-                ary_int_val[i] = 0;
-
-            for (int i = 0; i < ix; i++)
+            switch (e.Key)
             {
-                lst.Add(ary_int_val);
+                case Key.D1:
+                case Key.NumPad1:
+                    PerformClick2(BTN_1);
+                    break;
+                case Key.D2:
+                case Key.NumPad2:
+                    PerformClick2(BTN_2);
+                    break;
+                case Key.D3:
+                case Key.NumPad3:
+                    PerformClick2(BTN_3);
+                    break;
+                case Key.D4:
+                case Key.NumPad4:
+                    PerformClick2(BTN_4);
+                    break;
+                case Key.D5:
+                case Key.NumPad5:
+                    PerformClick2(BTN_5);
+                    break;
+                default:
+                    MessageBox.Show(e.Key.ToString());
+                    break;
             }
-        }
 
-
-        //*************************************************************************
-        public void CtrlEn(bool bl_Enable)
-        {
-            GridAnswerBtn.IsEnabled = bl_Enable;
-            GridList.IsEnabled = bl_Enable;
-            GridUpDown.IsEnabled = bl_Enable;
-
-            blRunSts = bl_Enable;
-        }
-
-
-
-        //*************************************************************************
-        private void Click_Debug(object sender, RoutedEventArgs e)
-        {
-            //SetList(ref cls_comm.LstAnswer, 10, 5);
-            CtrlEn(!GridAnswerBtn.IsEnabled);
         }
     }
 }
